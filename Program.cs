cc335c65cc94e084ec07f1aecdb3e9a238576304
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2024-03-26 16:37:40 +0000

    Ajustes no ApllicationDbContext

--- Program.cs
+++ Program.cs
@@ -1,25 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Data;
+using WebApplication2;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 26));
+
+var connectionString = builder.Configuration.GetConnectionString("MariaDB");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, serverVersion)
+);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetService<ApplicationDbContext>();
+
+    if (dbContext != null && dbContext.Database.GetDbConnection().State != ConnectionState.Open)
+    {
+        dbContext.Database.OpenConnection();
+
+        // Run EnsureCreated() to create the database and any pending migrations
+        dbContext.Database.EnsureCreated();
+    }
+
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseExceptionHandler("/Home/Error");
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
